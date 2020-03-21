096ba683ebdc982a979a675167c08b3605ec680e
Phung Pham <phung.tech@gmail.com>
2020-03-20 19:56:43 -0500

    Updates

---
+++ precache-manifest.4e38c141e5c77bfb975cd1ecffb249fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194a975ede010e5e705912ef1575f188",
+    "url": "/Beachwalk-resort-styledComponents/index.html"
+  },
+  {
+    "revision": "623358cc86713dd2bc3e",
+    "url": "/Beachwalk-resort-styledComponents/static/js/2.846e98a3.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/Beachwalk-resort-styledComponents/static/js/2.846e98a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096e549c4ae19d88e267",
+    "url": "/Beachwalk-resort-styledComponents/static/js/main.65bc564f.chunk.js"
+  },
+  {
+    "revision": "13098b56c2da6cf1ed0a",
+    "url": "/Beachwalk-resort-styledComponents/static/js/runtime-main.06bb1475.js"
+  },
+  {
+    "revision": "0e394d2533b6a364eef64e82e309feb8",
+    "url": "/Beachwalk-resort-styledComponents/static/media/aboutBcg.0e394d25.jpeg"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/Beachwalk-resort-styledComponents/static/media/homeBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "7577289d7512b6908b107a7a9658717f",
+    "url": "/Beachwalk-resort-styledComponents/static/media/room1.7577289d.jpeg"
+  },
+  {
+    "revision": "4e32e8c29c55148da633b6d83654796b",
+    "url": "/Beachwalk-resort-styledComponents/static/media/room2.4e32e8c2.jpeg"
+  },
+  {
+    "revision": "37780ac0f97ce693235c6dd6daec1508",
+    "url": "/Beachwalk-resort-styledComponents/static/media/room3.37780ac0.jpeg"
+  }
+]);
